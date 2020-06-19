7ee0bf0f5ccca805753b12731875393bd09603d8
cx-Zh <491907188@qq.com>
2020-06-19 21:25:27 +0800

    add loadgreet function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 import "./style/index.scss";
+
+const sayNewHello = () => "New Hello";
+document.writeln(sayNewHello());
+
+// eslint-disable-next-line no-unused-vars
+function LoadGreet() {
+  fetch("http://localhost:8080/greet").then(function (response) {
+    return response.json();
+  });
+}
